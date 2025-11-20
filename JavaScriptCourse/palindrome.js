ac184e013e3caf5f9cce31df1b595f71e7e5922e
Manoj4003 <manojknr16@gmail.com>
2025-11-20 23:27:37 +0530

    intro

--- JavaScriptCourse/palindrome.js
+++
@@ -1,7 +0,0 @@
-function isPalindrome(str) {
-    const rev = str.split("").reverse().join("");
-    return rev === str;
-}
-
-console.log(isPalindrome("madam")); // true
-console.log(isPalindrome("manoj")); // false
